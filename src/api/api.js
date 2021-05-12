57762a2830616a0bd2ea9524918a969bfbdd54f5
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-05-11 22:57:26 -0300

    changing to http weather api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const api = {
     weatherApi: {
       key: "d355671dd4ee8898100b7f1969362cd6",
-      base: "https://api.weatherstack.com/current",
+      base: "http://api.weatherstack.com/current",
     },
     unsplashApi: {
       key: "qh-b4-wEIqKayiSN-tKQ5JP8opkjnI6-YeoYqj_smnU",
